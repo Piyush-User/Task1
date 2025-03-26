1fe2f02aee96301ebf974b974a74647805b4cad6
Piyush-User <piyushagarwal0365@gmail.com>
2025-03-26 18:43:51 +0530

    Update api.js

--- my-project/src/utils/api.js
+++ my-project/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000';
+const URL = 'https://task1-backend-04em.onrender.com';
 // const API_URL = 'http://localhost:5000/tasks';
 const API_URL = `${URL}/tasks`;
 
